176f39900666f62e079d301aeae3c6eda22a3245
João Paulo Santarém <joao.santarem@dbccompany.com.br>
2020-05-13 06:01:00 -0300

    Add build folder

---
+++ build/precache-manifest.7bb8d68f4773a18d4ec393a081fd4cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cc17a592386a53e7943503d459b960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df34df6ea32ddfce9c2",
+    "url": "/static/css/main.9611d803.chunk.css"
+  },
+  {
+    "revision": "04139430b1c7aad9f2c6",
+    "url": "/static/js/2.8c58c7a3.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.8c58c7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df34df6ea32ddfce9c2",
+    "url": "/static/js/main.3737cdd1.chunk.js"
+  },
+  {
+    "revision": "84c7b12e5232cf1a37aa",
+    "url": "/static/js/runtime-main.a11a3398.js"
+  }
+]);
